cfc977f60f63e57b14ce6a86069e0fe3ccb84e18
Sujoy Giri <girisujoy630@gmail.com>
2024-03-17 23:49:37 +0530

    added destination detail api route

--- models/destination-detail.model.js
+++ models/destination-detail.model.js
@@ -1,1 +1,27 @@
-const {Schema,model} = require("mongoose");
+const { Schema, model } = require("mongoose");
+
+const destinationDetailSchema = new Schema({
+    name: String,
+    title: String,
+    text: String,
+    at_a_glance: {
+        text: String,
+        currency: String,
+        popular_drink: String,
+        tipping_custom: String,
+        must_eat: String,
+        power_up: String,
+        language: String,
+        getting_there: String,
+        weather: String,
+        best_time_to_visit: String,
+        place_map_image_url: String
+    },
+    things_to_do: {
+        content: String,
+        things_image_url: String
+    }
+});
+
+const destinationDetailModel = model('DestinationDetail', destinationDetailSchema, 'destination-details');
+module.exports = destinationDetailModel;
--- routes/destination.route.js
+++ routes/destination.route.js
@@ -1,6 +1,7 @@
 const destinationRouter = require("express").Router();
 
 const destinationModel = require("../models/destination.model");
+const destinationDetailModel = require("../models/destination-detail.model");
 
 // {name:{$regex:/.*.*/gi},routePath:{$regex:/.*Los.*/gi}}
 // let regEx = new RegExp(/.*('s').*/gi)
@@ -12,22 +13,28 @@ destinationRouter.get("/search-destination", async (req, res, next) => {
     res.json(foundDestinations);
 });
 
-destinationRouter.get("/all-destination",async (req,res,next) => {
-    let allDestination = await destinationModel.find({},{_id:0})
+destinationRouter.get("/all-destination", async (req, res, next) => {
+    let allDestination = await destinationModel.find({}, { _id: 0 });
     res.json(allDestination);
-})
+});
 
-destinationRouter.get("/destination-detail",async (req,res,next) => {
-    const {region,country,place} = req.query
-    if(region && country && place){
-        // let destinationDetail = await 
+destinationRouter.get("/destination-detail", async (req, res, next) => {
+    const { region, country, place } = req.query;
+    let destinationDetail = null;
+    if (region && country && place) {
+        destinationDetail = await destinationDetailModel.find({ name: place }, { _id: 0 });
     }
-    else if(region && country){
-
+    else if (region && country) {
+        destinationDetail = await destinationDetailModel.find({ name: country }, { _id: 0 });
     }
-    else if(region){
-
+    else if (region) {
+        destinationDetail = await destinationDetailModel.find({ name: region }, { _id: 0 });
     }
-})
+    if(destinationDetail[0]){
+        res.json(destinationDetail[0]);
+    }else{
+        res.status(400).json({success:false,message:'nothing found!'})
+    }
+});
 
 module.exports = destinationRouter;
